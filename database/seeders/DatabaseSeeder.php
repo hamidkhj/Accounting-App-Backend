4d3129ce15a780804092e10f85b230b6faa96962
grimlord <hkmygames1234@gmail.com>
2022-06-29 22:52:32 +0430

    uncomment seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,20 +15,20 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
         $this->call([
-            // PermissionsTableSeeder::class,
-            // RolesTableSeeder::class,
-            // ConnectRelationshipsSeeder::class,
-            // GroupSeeder::class,
-            // UsersSeeder::class,
-            // PackageTypeTableSeeder::class,
-            // TitleSeeder::class,
-            // MaritalStatusSeeder::class,
-            // ServiceSeeder::class,
-            // UserPackageSeeder::class,
+            PermissionsTableSeeder::class,
+            RolesTableSeeder::class,
+            ConnectRelationshipsSeeder::class,
+            GroupSeeder::class,
+            UsersSeeder::class,
+            PackageTypeTableSeeder::class,
+            TitleSeeder::class,
+            MaritalStatusSeeder::class,
+            ServiceSeeder::class,
+            UserPackageSeeder::class,
             ConnectionLogSeeder::class,
-            // ActionLogSeeder::class,
-            // CallsLogSeeder::class,
-            // PackageTableSeeder::class,
+            ActionLogSeeder::class,
+            CallsLogSeeder::class,
+            PackageTableSeeder::class,
         ]);
     }
 }
